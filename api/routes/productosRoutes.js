90844eb32beeea276ba8793d64ed1548022aed5c
steven <stevenvallejo780@gmail.com>
2021-06-03 01:02:32 -0500

    feat init docs

--- api/routes/productosRoutes.js
+++ api/routes/productosRoutes.js
@@ -1,14 +1,52 @@
 module.exports = function (app) {
   var productos = require("../controllers/productosController");
 
-  app
-    .route("/productos")
-    .get(productos.listar_productos)
-    .post(productos.crear_productos);
+  /**
+  @swagger
+  * path:
+  * /productos/:
+  *   get:
+  *    summary: Devuelve una lista de todos los productos.
+  *    description: Retrieve a list of users from JSONPlaceholder. Can be used to populate a list of fake users when prototyping or testing an API.
+  *    responses:
+  *      200:
+  *        description: A list of users.
+  *        content:
+  *          application/json:
+  *            schema:
+  *              type: object
+  *              properties:
+  *                data:
+  *                  type: array
+  *                  items:
+  *                    type: object
+  *                    properties:
+  *                      _id:
+  *                        type: integer
+  *                        description: Id de producto.
+  *                        example: 1
+  *                      nombre:
+  *                        type: string
+  *                        description: Nombre del producto.
+  *                        example: Camisa Verde
+  *                      descripcion:
+  *                        type: string
+  *                        description: Descripcion del producto.
+  *                        example: Camisa verde talla M
+  *                      caracteristicas:
+  *                        type: string
+  *                        description: Caracteracteristicas del producto.
+  *                        example: ["Talla": "M"]
+  *                      stock:
+  *                        type: integer
+  *                        description: Cantidad de productos.
+  *                        example: 10
+  */
 
-  app
-    .route("/productos/:productosId")
-    .get(productos.leer_producto)
-    .put(productos.actualizar_producto)
-    .delete(productos.eliminar_producto);
+  app.route("/productos").get(productos.listar_productos);
+  app.route("/productos").get(productos.listar_productos);
+  app.route("/productos").post(productos.crear_productos);
+  app.route("/productos/:productosId").get(productos.leer_producto);
+  app.route("/productos/:productosId").put(productos.actualizar_producto)
+  app.route("/productos/:productosId").delete(productos.eliminar_producto);
 };
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var express = require("express"),
   mongoose = require("mongoose"),
   Productos = require("./api/models/productosModel"), // Cargar el demolo de producto
   bodyParser = require("body-parser");
+  swaggerJsdoc = require("swagger-jsdoc"),
+  swaggerUi = require("swagger-ui-express");
 
 // Instancia de base de datos mongoDB con mongoose
 mongoose.Promise = global.Promise;
@@ -17,6 +19,38 @@ app.use(bodyParser.json());
 var routes = require("./api/routes/productosRoutes"); // Importar rutas de produtos
 routes(app); // Registrar rutas
 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Example Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Steven",
+        url: "https://www.linkedin.com/in/steven-vallejo-ortiz-430213155/",
+        email: "stevenvallejo@gmail.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/",
+      },
+    ],
+  },
+  apis: ["./api/routes/productosRoutes.js"],
+};
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
 app.listen(port);
 
 console.log("API UP in " + host + ":" + port);
